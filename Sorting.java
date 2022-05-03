75d357b0a342b307b1f5fbe8c063c595477e21ad
Daryl0101 <daryl.tneoh@gmail.com>
2022-05-03 13:24:48 +0800

    Second commit: radix sort done

--- Sorting.java
+++ Sorting.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.LinkedList;
 
@@ -23,19 +24,17 @@ public class Sorting {
         QuickSort(quick_sorted,0,quick_sorted.size()-1);
 
         // Radix Sort
-        /*LinkedList<String> radix_sorted = new LinkedList<>(data);
-        System.out.println(radix_sorted + "\n" + bubble_sorted);
-        System.out.println(radix_sorted.equals(bubble_sorted));*/
+        LinkedList<String> radix_sorted = new LinkedList<>(data);
+        RadixSort(radix_sorted);
     }
 
     // Import File
-    static LinkedList<String> ImportFile(File file){
+    private static LinkedList<String> ImportFile(File file){
         LinkedList<String> data = new LinkedList<>();
         try{
             Scanner fileReader = new Scanner(file);
-            while (fileReader.hasNextLine()) {
+            while (fileReader.hasNextLine())
                 data.add(fileReader.nextLine());
-            }
             fileReader.close();
         }catch (FileNotFoundException e){
             System.out.println("Error happened when importing file.");
@@ -45,43 +44,40 @@ public class Sorting {
     }
 
     // Insertion Sort Algorithm
-    static void InsertionSort(LinkedList<String> data){
+    public static void InsertionSort(LinkedList<String> data){
         String tmp;
         int outer, inner;
 
         for(outer=1; outer<data.size(); outer++){
             tmp = data.get(outer);
             for(inner=outer; inner>0; inner--){
-                if(tmp.compareTo(data.get(inner-1))<0){
+                if(tmp.compareTo(data.get(inner-1))<0)
                     data.set(inner, data.get(inner-1));
-                }
-                else {
+                else
                     break;
-                }
             }
             data.set(inner, tmp);
         }
     }
 
     // Bubble Sort Algorithm
-    static void BubbleSort(LinkedList<String> data){
+    public static void BubbleSort(LinkedList<String> data){
         int outer, inner;
 
         for(outer=data.size()-1; outer>0; outer--){
             for(inner=0; inner<outer; inner++){
-                if(data.get(inner).compareTo(data.get(inner+1))>0){
+                if(data.get(inner).compareTo(data.get(inner+1))>0)
                     swap(data, inner, outer);
-                }
             }
         }
     }
 
     // Quick Sort Algorithm
-    static void QuickSort(LinkedList<String> data, int left, int right){
+    public static void QuickSort(LinkedList<String> data, int left, int right){
         int pivot;
-        if(left>right){
+        if(left>right)
             return;
-        }else{
+        else{
             pivot = Partition(data,left,right);
             QuickSort(data,left,pivot-1);
             QuickSort(data,pivot+1,right);
@@ -89,7 +85,7 @@ public class Sorting {
     }
 
     // Partition Algorithm
-    static int Partition(LinkedList<String> data, int left, int right){
+    private static int Partition(LinkedList<String> data, int left, int right){
         String pivot_data = data.get(left);
         int i = left;
         for(int j=left+1;j<=right;j++){
@@ -103,9 +99,51 @@ public class Sorting {
     }
 
     // Utility function for swapping
-    static void swap(LinkedList<String> data, int i, int j){
+    private static void swap(LinkedList<String> data, int i, int j){
         String tmp = data.get(i);
         data.set(i, data.get(j));
         data.set(j, tmp);
+    }
+
+    // Radix Sort Algorithm
+    public static void RadixSort(LinkedList<String> data){
+        int maxIndex = 0;
+        for(int i=0;i<data.size();i++){
+            if(data.get(i).length()-1 > maxIndex){
+                maxIndex = data.get(i).length()-1;
+            }
+        }
+
+        for(int i=maxIndex;i>=0;i--){
+            countingSort(data,i,'a','z');
+        }
+    }
+
+    // Counting Sort Algorithm
+    private static void countingSort(LinkedList<String> data,int index,char lower,char upper){
+        int[] countArray = new int[(upper-lower)+2];
+        String[] tempArray = new String[data.size()];
+        Arrays.fill(countArray,0);
+
+        //increase count for char at index
+        for(int i=0;i<data.size();i++){
+            int charIndex = (data.get(i).length()-1 < index) ? 0 : ((data.get(i).charAt(index) - lower)+1);
+            countArray[charIndex]++;
+        }
+
+        //sum up countArray;countArray will hold last index for the char at each strings index
+        for(int i=1;i<countArray.length;i++){
+            countArray[i] += countArray[i-1];
+        }
+
+        for(int i=data.size()-1;i>=0;i--){
+            int charIndex = (data.get(i).length()-1 < index) ? 0 : (data.get(i).charAt(index) - lower)+1;
+            tempArray[countArray[charIndex]-1] = data.get(i);
+            countArray[charIndex]--;
+        }
+
+        for(int i=0;i<tempArray.length;i++){
+            data.set(i,tempArray[i]);
+        }
     }
 }
